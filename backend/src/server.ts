f9e94773140c678d8b67da3921d34f1b7ab4f046
Felix Hamann <felix.hamann@satellytes.com>
2022-04-24 19:23:38 +0200

    chore(backend): add cors to all routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import { Application } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors@v1.2.0/mod.ts';
 import { router } from './routes.ts';
 
 const app = new Application();
@@ -29,6 +30,8 @@ app.use(async (ctx, next) => {
   const ms = Date.now() - start;
   ctx.response.headers.set('X-Response-Time', `${ms}ms`);
 });
+
+app.use(oakCors());
 
 app.use(router.routes());
 app.use(router.allowedMethods());
