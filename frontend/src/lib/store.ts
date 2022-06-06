ccd4359718f4138d15150bbfdb1d1b4c85678efb
Felix Hamann <felix.hamann@satellytes.com>
2022-06-06 19:15:26 +0200

    chore: fix wrong import in store

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,6 +1,6 @@
 import { browser } from '$app/env';
 import { writable, type Readable, type Unsubscriber, type Writable } from 'svelte/store';
-import type { OfferPreview } from './offers';
+import type { OfferPreview } from '@types';
 
 export const isLoading = writable(false);
 export const isLoadingMore = writable(false);
