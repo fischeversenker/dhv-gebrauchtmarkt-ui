87a31c38cc8c56658d670a0d767c54ea7d8508e1
Felix Hamann <felix.hamann@satellytes.com>
2022-11-04 21:10:57 +0100

    chore: reduce cache max-age for single offers to one hour

--- backend/src/controllers/offers.ts
+++ backend/src/controllers/offers.ts
@@ -32,7 +32,7 @@ export const offersRouter = new Router()
     ) => response.json());
     const offerRawHtml = offerResponse.content;
     const offer = collectOffer(offerRawHtml, id);
-    context.response.headers.set('Cache-Control', 'max-age=604800');
+    context.response.headers.set('Cache-Control', 'max-age=3600');
     context.response.body = offer;
   })
   .post('/:id/contact', async (context) => {
