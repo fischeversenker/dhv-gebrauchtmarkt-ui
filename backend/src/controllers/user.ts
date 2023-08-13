f1b835a40c28dc2cdc3301518e9846c1f72e3c9f
Felix Hamann <felix.hamann@satellytes.com>
2023-08-13 20:09:21 +0200

    feat(backend): add /user/saved-search endpoints

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,27 +1,96 @@
-import { Router } from '../deps.ts';
-import { getLoggedInUser } from '../services/user.ts';
+import { Context, Router } from '../deps.ts';
+import { getLoggedInUser, User } from '../services/user.ts';
 
-export const userRouter = new Router().get('/', async (context) => {
+export const requireAuthMiddleware = async (
+  context: Context,
+  next: () => Promise<unknown>,
+) => {
   const sessionId = await context.cookies.get('dhvsid');
 
   if (!sessionId) {
-    context.response.status = 400;
+    context.response.status = 401;
+    context.response.body = 'Unauthorized';
+    await context.cookies.set('dhvsid', null, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
     return;
   }
 
   const user = await getLoggedInUser(sessionId).catch((_) => null);
 
-  if (user) {
-    context.response.body = {
-      username: user.Username,
-    };
+  if (!user) {
+    context.response.status = 401;
+    context.response.body = 'Unauthorized';
+    await context.cookies.set('dhvsid', null, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
     return;
   }
 
-  context.response.status = 400;
-  await context.cookies.set('dhvsid', null, {
-    httpOnly: true,
-    sameSite: 'none',
-    secure: true,
+  context.state.user = user;
+  await next();
+};
+
+interface SavedSearch {
+  id: string;
+}
+
+export const userRouter = new Router<{ user: User }>()
+  .use(requireAuthMiddleware)
+  .get('/', (context) => {
+    context.response.body = context.state.user;
+  })
+  .get('/saved-search', async (context) => {
+    const kv = await Deno.openKv();
+    context.response.body = await kv.get([
+      'users',
+      context.state.user.userID,
+      'savedSearches',
+    ]);
+  })
+  .post('/saved-search', async (context) => {
+    const newSavedSearch = await context.request.body().value;
+    newSavedSearch.id = Math.random().toString(36).substring(2, 9);
+
+    const kv = await Deno.openKv();
+    const existingSavedSearches = await kv.get<SavedSearch[]>([
+      'users',
+      context.state.user.userID,
+      'savedSearches',
+    ]);
+
+    await kv.set(
+      ['users', context.state.user.userID, 'savedSearches'],
+      [newSavedSearch, ...(existingSavedSearches.value ?? [])],
+    );
+
+    const res = await kv.get([
+      'users',
+      context.state.user.userID,
+      'savedSearches',
+    ]);
+    context.response.body = res;
+  })
+  .delete('/saved-search/:id', async (context) => {
+    const kv = await Deno.openKv();
+
+    const existingSavedSearches = await kv.get<SavedSearch[]>([
+      'users',
+      context.state.user.userID,
+      'savedSearches',
+    ]);
+    // TODO: throw an error if id not found
+    const newSavedSearches = existingSavedSearches.value?.filter(
+      (s) => s.id !== context.params.id,
+    );
+    await kv.set(
+      ['users', context.state.user.userID, 'savedSearches'],
+      newSavedSearches ?? [],
+    );
+
+    context.response.body = [];
   });
-});
--- backend/src/deps.ts
+++ backend/src/deps.ts
@@ -1,4 +1,4 @@
-export { Application, Router } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
+export { Application, Context, Router } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
 export { oakCors } from 'https://deno.land/x/cors@v1.2.0/mod.ts';
 export { DOMParser, Element } from 'https://deno.land/x/deno_dom@v0.1.22-alpha/deno-dom-wasm.ts';
 export { load as loadEnvVars } from 'https://deno.land/std@0.177.0/dotenv/mod.ts';
